5fadaffb6828527ad48f4a5395a6e837b76a0280
Thomas Swisher <thomas@structuredseo.com>
2021-04-12 05:53:48 -0600

    Added  Clock

--- app/Models/Quote.js
+++ app/Models/Quote.js
@@ -2,13 +2,13 @@ export default class Quote {
     constructor({ content, author }) {
         this.quote = content
         this.author = author
-        console.log(content)
+        //console.log(content)
     }
 
     get Template() {
 
         return /*html*/`
-                       <div class="col-12 hover-div quote">${this.quote}</div>
+                       <div class="col-12 hover-div quote d-flex justify-content-center">${this.quote}</div>
                        <div class="col-12 hidden-text">${this.author}</div>
                 `
     }
